332e648603497a230c8bc021edc22405cb4c5edf
miaaae <137583420+miaaae@users.noreply.github.com>
2024-01-16 22:12:39 +0900

    Update script.js - console.log

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const appbar=$('.app-bar-wrap .appbarBt');
 const mobileMenu=$('.mobile-menu')
 const appbarCloseBt=$('.appbarCloseBt')
 
+console.log('침투했다!');
+
 appbar.on('click', function(){
     mobileMenu.stop().animate({left: 0},500);
     $('.header-mobile-menu').stop().animate({left:'100%'},500)
